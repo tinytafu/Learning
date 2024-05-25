7438bfe90ed44d99ef6066b242f694cb90ca1650
tytofu <wsccie@icloud.com>
2024-05-25 23:10:15 +0800

    -feat:update mycmd.cc

---
+++ Linux/Whb/Lesson_17/mycmd.cc
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+int main () {
+  cout << "PATH: " << getenv("PATH") << endl;
+  cout << "wangshuai: " << getenv("wangshuai") << endl;
+  cout << "hello cpp" << endl;
+  cout << "hello cpp" << endl;
+  cout << "hello cpp" << endl;
+  cout << "hello cpp" << endl;
+  cout << "hello cpp" << endl;
+  cout << "hello cpp" << endl;
+  return 16;
+}
