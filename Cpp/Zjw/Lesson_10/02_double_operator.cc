6369483fcdd4228771e64da64beda5d34de2d1a5
tytofu <wsccie@icloud.com>
2024-05-31 23:54:25 +0800

    -feat:add 02_double_operator.cc

---
+++ Cpp/Zjw/Lesson_10/02_double_operator.cc
@@ -0,0 +1,52 @@
+// 运算符双目操作符重载
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::string;
+class human;
+human operator+(const human &l, const human &r);
+human operator-(const human &l, const human &r);
+class human {
+public:
+  human(const int f_age = 0, const char *f_name = "")
+      : m_age(f_age), m_name(f_name) {}
+  void getinfo() {
+    cout << "m_age is " << m_age << endl;
+    cout << "m_name is " << m_name << endl;
+  }
+  // /*成员加法运算符重载函数*/
+  // human operator+(const human &that) const {
+  //   /*只想让它们的年龄相加,名字取首字母加在一起*/
+  //   return (human(this->m_age + that.m_age,
+  //                 (this->m_name + "+" + that.m_name).c_str()));
+  // }
+  // /*成员减法运算符重载函数*/
+  // human operator-(const human &that) const {
+  //   /*只想让它们的年龄相加,名字取首字母加在一起*/
+  //   return (human(this->m_age - that.m_age,
+  //                 (this->m_name + "-" + that.m_name).c_str()));
+  // }
+
+private:
+  int m_age;
+  string m_name;
+  friend human operator+(const human &l, const human &r);
+  friend human operator-(const human &l, const human &r);
+};
+
+human operator+(const human &l, const human &r) {
+  return human(l.m_age + r.m_age, (l.m_name + "+" + r.m_name).c_str());
+}
+human operator-(const human &l, const human &r) {
+  return human(l.m_age - r.m_age, (l.m_name + "-" + r.m_name).c_str());
+}
+
+int main() {
+
+  human h1(10, "wangshuai"), h2(20, "huamengjun");
+  human res_add = h1 + h2;
+  human res_sub = h1 - h2;
+  res_sub.getinfo();
+  res_add.getinfo();
+}
