5ab7551e3a298c1aff413f84a11dee98502319e5
tytofu <wsccie@icloud.com>
2024-06-04 23:03:58 +0800

    -feat:update myfile.c

--- Linux/Whb/Lesson_20/myfile.c
+++ Linux/Whb/Lesson_20/myfile.c
@@ -1,17 +1,17 @@
 #include <fcntl.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 int main() {
-  /*关闭stdout*/
+
   umask(0);
-  int fd = open("log.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
+  // 打开一个文件
+  int fd = open("log.txt", O_WRONLY | O_CREAT | O_APPEND, 0666);
+  // 利用fprintf实现追加重定向将stdout先关闭
   close(1);
-  int ret = dup2(fd, 1);
-  // write(1, "我是wangshuai", sizeof("我是wangshuai"));
-  fprintf(stdout, "我是wangshuai ret is %d\n", ret);
-  close(fd);
+  dup2(fd, 1);
+
+  fprintf(stdout, "我是王帅,我在做追加重定向! fd = %d\n", fd);
 }
