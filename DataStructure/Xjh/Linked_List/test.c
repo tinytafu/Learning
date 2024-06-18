3e91d81801248685cb4d19c3abc81f02133eccf6
tytofu <wsccie@icloud.com>
2024-06-18 23:49:31 +0800

    -feat:update test.c

--- DataStructure/Xjh/Linked_List/test.c
+++ DataStructure/Xjh/Linked_List/test.c
@@ -2,16 +2,18 @@
 #include <stdio.h>
 
 int main() {
-  // 创建一个指向链表头结点的指针
+  // 创建一个无结点的链表
   node_t *head = NULL;
-  // 尾部插入三个结点
-  for (int i = 0; i < 5; i++) {
-    push_back_linked_list(&head, i);
-  }
-  traversal_linked_list(head);
-  for (int i = 0; i < 4; i++)
-    pop_back_linked_list(&head);
-  traversal_linked_list(head);
-  pop_back_linked_list(&head);
+  // 插入三个数据:1,2,3
+  push_back_linked_list(&head, 1);
+  push_back_linked_list(&head, 2);
+  push_back_linked_list(&head, 3);
+
+  // 获取链表长度
+  unsigned short length = 0;
+  length_linked_list(head, &length);
+  printf("length is %hd\n", length);
+
+  // 打印链表中每个结点的数据
   traversal_linked_list(head);
 }
