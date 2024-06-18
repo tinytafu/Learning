3257c818ebf082a85793f7eb3ab78be9946c8479
tytofu <wsccie@icloud.com>
2024-06-18 23:49:15 +0800

    -feat:update linked_list.h

--- DataStructure/Xjh/Linked_List/linked_list.h
+++ DataStructure/Xjh/Linked_List/linked_list.h
@@ -3,12 +3,6 @@
 
 typedef struct node node_t;
 typedef int elem_t;
-
-typedef enum {
-  FALSE,
-  TRUE,
-} bool_t;
-
 // 定义一个结构体类型表示链表中的结点类型
 struct node {
   // 结点中的数据
@@ -18,12 +12,26 @@ struct node {
 };
 
 #endif
+/*-----OTHER-----*/
 
 /*遍历链表中结点的数据*/
 void traversal_linked_list(node_t *head);
 
+/*计算链表的长度*/
+void length_linked_list(node_t *head, unsigned short *length);
+
+/*-----OTHER-----*/
+
+/*-----Insert-----*/
+
 /*将新结点插入到链表的尾部*/
-bool_t push_back_linked_list(node_t **head, elem_t data);
+void push_back_linked_list(node_t **head, elem_t data);
+
+/*-----INSERT-----*/
+
+/*-----DELETE-----*/
 
 /*删除链表中的尾部元素*/
-bool_t pop_back_linked_list(node_t **head);
+void pop_back_linked_list(node_t **head);
+
+/*-----DELETE-----*/
