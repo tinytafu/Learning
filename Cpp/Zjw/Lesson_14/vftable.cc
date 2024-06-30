f64b21c2fff0a1fab86daef5cc6f3baf241fa10d
tytofu <wsccie@icloud.com>
2024-07-01 00:07:15 +0800

    -feat:add vftable.cc

--- Cpp/Zjw/Lesson_14/vftable.cc
+++ Cpp/Zjw/Lesson_14/vftable.cc
@@ -13,6 +13,8 @@ public:
     cout << "A:dou_vfunc" << endl;
     return 1.1;
   }
+  int m_a;
+  int m_b;
 };
 class B : public A {
 public:
@@ -20,9 +22,14 @@ public:
 };
 
 int main() {
+  int var = 10;
+  double dou = 20.2;
   A a;
   B b;
   cout << sizeof(a) << endl;
   cout << sizeof(b) << endl;
   // 如何拿到A类中指向虚函数表的指针
+  int (**pfunc)(const int *p) = *(int (***)(const int *P))(&a);
+  pfunc[0](&var);
+  ((double (**)(const double *p))(pfunc + 1))[0](&dou);
 }
