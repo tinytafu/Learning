b1a2b188239958e29beae23d11e767e4c266cc5f
tytofu <wsccie@icloud.com>
2024-07-09 09:47:25 +0800

    -feat:update Zjw/Lesson_18/decltype_three.cc

--- Cpp/Zjw/Lesson_18/decltype_three.cc
+++ Cpp/Zjw/Lesson_18/decltype_three.cc
@@ -1,7 +1,9 @@
 // 返回值类型后置
 #include <iostream>
-#include <typeinfo>
 using namespace std;
+// decltype是一个编译时操作符,用于获取表达式的类型.不会实际执行表达式,只是用来检查表达式的类型
+// 因此这里的警告是在未求值的上下文中,new
+// char{'a'}这个表达式是有副作用的,但是在此操作符作用下 是没作用的
 auto func() -> decltype(new char{'a'}) { return new char(); }
 int main() {
   decltype(func()) ptr = func();
